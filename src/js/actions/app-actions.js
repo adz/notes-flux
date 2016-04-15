69c690a9a34faf9283483a432aec8b1e73f45a43
Adam Davies <adzdavies@gmail.com>
2016-04-15 11:53:20 +1000

    Demonstrate optimistic vs pessimistic saves

--- src/js/actions/app-actions.js
+++ src/js/actions/app-actions.js
@@ -7,6 +7,17 @@ export default {
     dispatch({
       actionType: AppConstants.ADD_NOTE, note
     })
+    // Optimisitically save -- already added to UI by above dispatch
+    API.saveNote(note).catch(() =>
+      this.noteAddFailed(note) // have to write failure action / handling in store
+    )
+  },
+
+  // alternative method: pessimistic - don't change UI until save is OK
+  saveNote(note) {
+    API.saveNote(note)
+      .then(() => this.addNote(note))
+      .catch(() => this.showError(`Couln't save note ${note}`))
   },
 
   getNotes: function() {
